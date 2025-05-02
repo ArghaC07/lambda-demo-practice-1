52365d69f6e42bd17588aefd49bc2bdf8b1a0556
Arghadee <argha1032@gmail.com>
2025-05-03 00:17:54 +0530

    Added buildspec.yml file for CICD

---
+++ lambda_function.py
@@ -0,0 +1,28 @@
+import json
+import boto3
+from datetime import datetime
+import logging
+import os
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    s3_client = boto3.client('s3')
+    source_bucket_name = 'source-bucket-4302'
+    destination_bucket_name = 'target-bucket-4302'
+    response = s3_client.list_objects_v2(Bucket=source_bucket_name)
+    if 'Contents' in response:
+        logger.info('Inside Contents') 
+        for obj in response['Contents']:
+            if obj['Key'].endswith('.csv'):
+                logger.info('CSV File found in source bucket')
+                s3_client.copy_object(
+                    Bucket = destination_bucket_name,
+                    CopySource = {'Bucket': source_bucket_name, 'Key': obj['Key']},
+                    Key = os.path.splitext(obj['Key'])[0] 
+                          + '_' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') 
+                          + '.csv' )
+                s3_client.delete_object(Bucket=source_bucket_name, Key=obj['Key'])
+                logger.info('CSV File moved to target bucket')
+ 
